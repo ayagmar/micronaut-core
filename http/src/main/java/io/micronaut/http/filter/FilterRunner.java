8e61d4e6024406e9c43b3e965b703bb9a625fd2c
Denis Stepanov <denis.s.stepanov@oracle.com>
2025-04-07 14:31:52 +0200

    PreMarching filter errors should be filtered as ordinary request filters

--- http/src/main/java/io/micronaut/http/filter/FilterRunner.java
+++ http/src/main/java/io/micronaut/http/filter/FilterRunner.java
@@ -234,7 +234,8 @@ public class FilterRunner {
             } else {
                 // Pre-matching filters plus route match resolver
                 var f = new RouteMatchResolverHttpFilter();
-                filtersToRun.add(f);
+                filtersToRun.add(0, f); // Response filter to resolve filters on error
+                filtersToRun.add(f); // Request filter to resolve filters after preMatch filters
                 iterator = filtersToRun.listIterator();
                 f.filterIterator = iterator;
             }
@@ -396,6 +397,12 @@ public class FilterRunner {
     final class RouteMatchResolverHttpFilter implements InternalHttpFilter {
 
         private ListIterator<InternalHttpFilter> filterIterator;
+        private boolean executed = false;
+
+        @Override
+        public boolean isFiltersResponse() {
+            return true;
+        }
 
         @Override
         public boolean isFiltersRequest() {
@@ -403,7 +410,22 @@ public class FilterRunner {
         }
 
         @Override
+        public ExecutionFlow<FilterContext> processResponseFilter(FilterContext context, Throwable exceptionToFilter) {
+            if (!executed) {
+                cleanupAllNext();
+                addFoundFilters(context.request());
+                moveIteratorToEnd();
+                return ExecutionFlow.just(context);
+            }
+            return ExecutionFlow.just(context);
+        }
+
+        @Override
         public ExecutionFlow<FilterContext> processRequestFilter(FilterContext context) {
+            if (filterIterator.nextIndex() == 1) {
+                return ExecutionFlow.just(context);
+            }
+            executed = true;
             HttpRequest<?> request = context.request();
             try {
                 doRouteMatch(request);
@@ -411,18 +433,44 @@ public class FilterRunner {
             } catch (Throwable throwable) {
                 return processFailurePropagateException(throwable, context);
             } finally {
+                cleanupAllPrevious();
+                addFoundFilters(request);
+                moveIteratorToBeginning();
+            }
+        }
+
+        private void moveIteratorToBeginning() {
+            while (filterIterator.hasPrevious()) {
+                filterIterator.previous();
+            }
+        }
+
+        private void moveIteratorToEnd() {
+            while (filterIterator.hasNext()) {
+                filterIterator.next();
+            }
+        }
+
+        private void addFoundFilters(HttpRequest<?> request) {
+            List<InternalHttpFilter> postFilters = findInternalFiltersAfterRouteMatch(request);
+            for (InternalHttpFilter postFilter : postFilters) {
+                filterIterator.add(postFilter);
+            }
+        }
+
+        private void cleanupAllNext() {
+            filterIterator.remove();
+            while (filterIterator.hasNext()) {
+                filterIterator.next();
                 filterIterator.remove();
-                while (filterIterator.hasPrevious()) {
-                    filterIterator.previous();
-                    filterIterator.remove();
-                }
-                List<InternalHttpFilter> postFilters = findInternalFiltersAfterRouteMatch(request);
-                for (InternalHttpFilter postFilter : postFilters) {
-                    filterIterator.add(postFilter);
-                }
-                while (filterIterator.hasPrevious()) {
-                    filterIterator.previous();
-                }
+            }
+        }
+
+        private void cleanupAllPrevious() {
+            filterIterator.remove();
+            while (filterIterator.hasPrevious()) {
+                filterIterator.previous();
+                filterIterator.remove();
             }
         }
     }
