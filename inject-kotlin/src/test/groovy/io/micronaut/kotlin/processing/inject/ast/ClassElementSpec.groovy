5ec884d077c01eaf7bdc715186e00a0c503ef632
Denis Stepanov <denis.s.stepanov@oracle.com>
2025-04-14 13:42:39 +0200

    Progress

--- inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/inject/ast/ClassElementSpec.groovy
+++ inject-kotlin/src/test/groovy/io/micronaut/kotlin/processing/inject/ast/ClassElementSpec.groovy
@@ -859,8 +859,8 @@ class Test<T : Test<T>>
     }
 
     void "test recursive generic method return"() {
-        expect:
-            buildClassElement('test.MyFactory', '''\
+        given:
+            ClassElement ce = buildClassElementTransformed('test.MyFactory', '''\
 package test
 
 import org.hibernate.SessionFactory
@@ -876,18 +876,22 @@ class MyFactory {
 ''') {
                 def sessionFactoryMethod = it.findMethod("sessionFactory").get()
                 def withOptionsMethod = sessionFactoryMethod.getReturnType().findMethod("withOptions").get()
-                assert withOptionsMethod.getReturnType().getTypeArguments()
-                def typeArguments = withOptionsMethod.getReturnType().getTypeArguments()
-                assert typeArguments.size() == 1
-                def typeArgument = typeArguments.get("T")
-                assert typeArgument.name == "org.hibernate.SessionBuilder"
-                def nextTypeArguments = typeArgument.getTypeArguments()
-                def nextTypeArgument = nextTypeArguments.get("T")
-                assert nextTypeArgument.name == "org.hibernate.SessionBuilder"
-                def nextNextTypeArguments = nextTypeArgument.getTypeArguments()
-                def nextNextTypeArgument = nextNextTypeArguments.get("T")
-                assert nextNextTypeArgument.name == "java.lang.Object"
+                withOptionsMethod.getReturnType().getTypeArguments()
+                return it
             }
+        expect:
+            def sessionFactoryMethod = ce.findMethod("sessionFactory").get()
+            def withOptionsMethod = sessionFactoryMethod.getReturnType().findMethod("withOptions").get()
+            def typeArguments = withOptionsMethod.getReturnType().getTypeArguments()
+            typeArguments.size() == 1
+            def typeArgument = typeArguments.get("T")
+            typeArgument.name == "org.hibernate.SessionBuilder"
+            def nextTypeArguments = typeArgument.getTypeArguments()
+            def nextTypeArgument = nextTypeArguments.get("T")
+            nextTypeArgument.name == "org.hibernate.SessionBuilder"
+            def nextNextTypeArguments = nextTypeArgument.getTypeArguments()
+            def nextNextTypeArgument = nextNextTypeArguments.get("T")
+            nextNextTypeArgument.name == "java.lang.Object"
     }
 
     void "test recursive generic method return 2"() {
